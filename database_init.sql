1a93f1aaf2e8cd0eaaa7e42514a90e4fa3d51dc2
Renerine <milionevincent@gmail.com>
2019-12-30 14:14:57 +0100

    add user to database

--- database_init.sql
+++ database_init.sql
@@ -372,6 +372,7 @@ CREATE TABLE IF NOT EXISTS `trawell`.`user` (
   UNIQUE INDEX `userName_UNIQUE` (`userName` ASC))
 ENGINE = InnoDB;
 
+INSERT INTO `user` VALUES (1,'umbertorussomando@gmail.com','prova','09F43236BB5E2B75230E705C39EDBB71','Umberto','Russomando','1998-12-11 00:00:00',0,NULL,0,'3347877736',0,0);
 
 -- -----------------------------------------------------
 -- Table `trawell`.`AgencyData`
